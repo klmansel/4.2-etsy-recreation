3b43dd1c3b2c46137b78468191651e76df9f74c7
Kara Mansel <callmedevmaybe@gmail.com>
2016-06-09 16:37:31 -0400

    linked etsy data

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,22 +1,72 @@
 var handlebars = require('handlebars');
+var _ = require('underscore');
 var $ = require('jquery');
 
 var url = 'https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=flasks&includes=Images,Shop&sort_on=score';
 
-function logData(data) {
-        console.log(data);
+
+fetchJSONP(url, function(data) {
+  var productList = data.results;
+
+  var source = $('#items-images').html();
+  var template = handlebars.compile(source);
+
+  productList.forEach(function(product){
+    console.log(product);
+    var renderTemplate = template(product);
+    $('.products-container').append(renderTemplate);
+  });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*
+        (url: String, callback: Function) -> undefined
+
+        Execute a callback function with the JSON results from the url specified.
+
+        Examples
+            var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=tacos&includes=Images,Shop";
+
+            fetchJSONP(url, function(data) {
+              // do something with data
+            });
+
+            // OR
+
+            function logData(data) {
+              console.log(data);
+            }
+
+            fetchJSONP(url, logData);
+      */
+      function fetchJSONP(url, callback) {
+          var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+          var script = document.createElement('script');
+
+          window[callbackName] = function(data) {
+              delete window[callbackName];
+              document.body.removeChild(script);
+              callback(data);
+          };
+
+          script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+          document.body.appendChild(script);
       }
-
-function fetchJSONP(url, data) {
-    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    var script = document.createElement('script');
-
-    window[callbackName] = function(data) {
-        delete window[callbackName];
-        document.body.removeChild(script);
-        callback(data);
-    };
-
-    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-    document.body.appendChild(script);
-}
